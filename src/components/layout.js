2c9d83a698272b583fa32da980435286c9e2fa18
chrisae9 <chrisae9@gmail.com>
2021-04-20 21:15:40 -0400

    Edited webpage

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,35 +1,34 @@
 import PropTypes from "prop-types";
 import React from "react";
 
-import Header from "./header";
+// import Header from "./header";
 
 function Layout({ children }) {
   return (
-    <div className="flex flex-col min-h-screen font-sans text-gray-900">
-      <Header />
+    <div className="flex flex-col min-h-screen font-sans text-gray-900 bg-gradient-to-b from-blue-400 to-blue-700">
 
-      <main className="flex-1 w-full max-w-4xl px-4 py-8 mx-auto md:px-8 md:py-16">
+      <main className="flex-auto w-full max-w-4xl px-4 py-8 mx-auto md:px-8 md:py-16">
         {children}
       </main>
 
-      <footer className="bg-blue-700">
+      <footer>
         <nav className="flex justify-between max-w-4xl p-4 mx-auto text-sm md:p-8">
           <p className="text-white">
             Created by{` `}
             <a
               className="font-bold no-underline"
-              href="https://bryant.io"
+              href="https://chis.dev"
               target="_blank"
               rel="noopener noreferrer"
             >
-              Taylor Bryant
+              Chris Alves
             </a>
           </p>
 
           <p>
             <a
               className="font-bold text-white no-underline"
-              href="https://github.com/taylorbryant/gatsby-starter-tailwind"
+              href="https://github.com/chrisae9"
               target="_blank"
               rel="noopener noreferrer"
             >
